af2ef19c765471e0a184f8175cab767031735952
snehalk06 <snehalkale943@gmail.com>
2026-02-01 00:29:21 +0530

    Add files via upload

---
+++ CP-Module/Assignments/Assignment5/que1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+	for(int row=1;row<=5;row++){
+		for(int col=1;col<=5;col++){
+			printf("* ");
+		}printf("\n");
+	}
+}
---
+++ CP-Module/Assignments/Assignment5/que3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	for(int row=1;row<=5;row++){
+		for(int col=5;col>=row;col--){
+			printf("* ");
+		}printf("\n");
+	}
+}
