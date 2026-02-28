efca82a0a972ca9189679112080f0f360ab3b7ca
snehalk06 <snehalkale943@gmail.com>
2026-02-28 23:14:19 +0530

    Delete CP-Module/Dashboard directory

--- CP-Module/Dashboard/script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    var role = document.getElementById("role").value;
-
-    if (role === "admin") {
-        window.location.href = "admin-dashboard.html";
-    } else {
-        window.location.href = "counselor-dashboard.html";
-    }
-}
