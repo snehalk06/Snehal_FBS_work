18daa6096e3f345ae787692fe8aa55f67e31b048
snehalk06 <snehalkale943@gmail.com>
2026-04-28 23:30:56 +0530

    Add files via upload

---
+++ Core Java-Module/Assignments/Java Assignments/Inheritance/PersonHierarchy.java
@@ -0,0 +1,223 @@
+class Person {
+
+    String name, gender;
+    int age, mobNum;
+
+    public Person() {
+        this.name = "Not Given";
+        this.gender = "Not Given";
+        this.age = 0;
+        this.mobNum = 0;
+    }
+
+    public Person(String name, String gender, int age, int mobNum) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.mobNum = mobNum;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    void setMobNum(int mobNum) {
+        this.mobNum = mobNum;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getGender() {
+        return this.gender;
+    }
+
+    int getAge() {
+        return this.age;
+    }
+
+    int getMobNum() {
+        return this.mobNum;
+    }
+
+    void display() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Age: " + this.age);
+        System.out.println("Mob No: " + this.mobNum);
+    }
+
+}
+
+class Student extends Person {
+
+    int rollNUM;
+    double cgpa, feesPaid;
+
+    public Student() {
+        super();
+        this.rollNUM = 0;
+        this.cgpa = 0;
+        this.feesPaid = 00;
+    }
+
+    public Student(int rollNUM, double cgpa, double feesPaid, String name, String gender, int age, int mobNum) {
+        super(name, gender, age, mobNum);
+        this.rollNUM = rollNUM;
+        this.cgpa = cgpa;
+        this.feesPaid = feesPaid;
+    }
+
+    void setRollNum(int rollNum) {
+        this.rollNUM = rollNum;
+    }
+
+    void setFeesPaid(double feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+
+    void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    int getRollNum() {
+        return this.rollNUM;
+    }
+
+    double getFeesPaid() {
+        return this.feesPaid;
+    }
+
+    double getCgpa() {
+        return this.cgpa;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Roll Num: " + rollNUM);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Fees Paid: " + feesPaid);
+    }
+
+}
+
+class Teacher extends Person {
+
+    int teacherId;
+    String subject;
+
+    public Teacher() {
+        super();
+        this.teacherId = 0;
+        this.subject = "Not Given";
+    }
+
+    public Teacher(int teacherId, String subject, String name, String gender, int age, int mobNum) {
+        super(name, gender, age, mobNum);
+        this.teacherId = teacherId;
+        this.subject = subject;
+    }
+
+    void setTeacherId(int id) {
+        this.teacherId = id;
+    }
+
+    void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    int getTeacherId() {
+        return this.teacherId;
+    }
+
+    String getSubject() {
+        return this.subject;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Teacher ID: " + teacherId);
+        System.out.println("Subject: " + subject);
+    }
+}
+
+class Employee extends Person {
+
+    int empId;
+    String dept;
+    double salary;
+
+    public Employee() {
+        super();
+        this.empId = 0;
+        this.dept = "Not Given";
+        this.salary = 0;
+    }
+
+    public Employee(int empId, String dept, double salary, String name, String gender, int age, int mobNum) {
+        super(name, gender, age, mobNum);
+        this.empId = empId;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    void setEmpId(int id) {
+        this.empId = id;
+    }
+
+    void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    int getEmpId() {
+        return this.empId;
+    }
+
+    String getDept() {
+        return this.dept;
+    }
+
+    double getSalary() {
+        return this.salary;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Department: " + dept);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class PersonHierarchy {
+
+    public static void main(String[] args) {
+
+        Student s = new Student(12, 8.15, 53000, "Pratik", "M", 21, 860072);
+        s.display();
+
+        System.out.println("");
+
+        Employee e = new Employee(1, "HR", 45000, "Snehal", "F", 25, 3465776);
+        e.display();
+
+         System.out.println("");
+         
+        Teacher t = new Teacher(2, "Core Java", "Vinayak Sir", "M", 35, 56987867);
+        t.display();
+
+    }
+}
---
+++ Core Java-Module/Assignments/Java Assignments/Inheritance/ShapeHierarchy.java
@@ -0,0 +1,152 @@
+class Shape {
+
+    double area;
+
+    public Shape() {
+        this.area = 00;
+    }
+
+    public Shape(double area) {
+        this.area = area;
+    }
+
+    void setArea(double area) {
+        this.area = area;
+    }
+
+    double getArea() {
+        return this.area;
+    }
+
+    void display() {
+        System.out.println("Area: " + this.area);
+    }
+}
+
+class Circle extends Shape {
+
+    double radius;
+
+    public Circle() {
+        super();
+        this.radius = 00;
+    }
+
+    public Circle(double radius, double area) {
+        super(area);
+        this.radius = radius;
+    }
+
+    void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    double getRadius() {
+        return this.radius;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Radius: " + radius);
+    }
+}
+
+class Rectangle extends Shape {
+
+    double length, breadth;
+
+    public Rectangle() {
+        super();
+        this.length = 00;
+        this.breadth = 00;
+    }
+
+    public Rectangle(double area, double length, double breadth) {
+        super(area);
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    void setLength(double length) {
+        this.length = length;
+    }
+
+    void setBreadth(double breadth) {
+        this.breadth = breadth;
+    }
+
+    double getLength() {
+        return this.length;
+    }
+
+    double getBreadth() {
+        return this.breadth;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Length: " + length);
+        System.out.println("Breadth: " + breadth);
+    }
+
+}
+
+class Triangle extends Shape {
+
+    double base, height;
+
+    public Triangle() {
+        super();
+        this.base = 00;
+        this.height = 00;
+    }
+
+    public Triangle(double area, double base, double height) {
+        super(area);
+        this.base = base;
+        this.height = height;
+    }
+
+    void setBase(double base) {
+        this.base = base;
+    }
+
+    void setHeight(double height) {
+        this.height = height;
+    }
+
+    double getBase() {
+        return this.base;
+    }
+
+    double getHeight() {
+        return this.height;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Base: " + base);
+        System.out.println("Height: " + height);
+    }
+
+}
+
+public class ShapeHierarchy {
+
+    public static void main(String[] args) {
+
+        Circle c = new Circle(4, 5.10);
+        c.display();
+
+        System.out.println(".()");
+
+        Triangle t = new Triangle(23, 4.5, 2.3);
+        t.display();
+
+        System.out.println(".()");
+
+        Rectangle r = new Rectangle(30, 3.7, 7.8);
+        r.display();
+
+    }
+}
