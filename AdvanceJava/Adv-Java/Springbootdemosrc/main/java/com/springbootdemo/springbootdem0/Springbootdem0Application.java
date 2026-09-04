32275656179def76b603b4e80c8003e03fbdc7c6
Snehal Kale <snehalkale943@gmail.com>
2026-09-04 23:22:33 +0530

    Add files via upload

---
+++ AdvanceJava/Adv-Java/Springbootdemosrc/main/java/com/springbootdemo/springbootdem0/Springbootdem0Application.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.springbootdem0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdem0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootdem0Application.class, args);
+	}
+
+}
---
+++ AdvanceJava/Adv-Java/Springbootdemosrc/test/java/com/springbootdemo/springbootdem0/Springbootdem0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.springbootdem0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdem0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
